a6112b75b76668cecc8784523ce267681f16b475
yatheendra28 <ryatheendra28@gmail.com>
2026-06-06 14:52:52 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -1,11 +1,11 @@
-yum install java-17-amazon-corretto -y
-wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.104/bin/apache-tomcat-9.0.104.tar.gz
-tar -zxvf apache-tomcat-9.0.104.tar.gz
-sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-9.0.104/conf/tomcat-users.xml
-sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-9.0.104/conf/tomcat-users.xml
-sed -i '58  a\<user username="tomcat" password="admin@123" roles="manager-gui, manager-script"/>' apache-tomcat-9.0.104/conf/tomcat-users.xml
-sed -i '59  a\</tomcat-users>' apache-tomcat-9.0.104/conf/tomcat-users.xml
-sed -i '56d' apache-tomcat-9.0.104/conf/tomcat-users.xml
-sed -i '21d' apache-tomcat-9.0.104/webapps/manager/META-INF/context.xml
-sed -i '22d'  apache-tomcat-9.0.104/webapps/manager/META-INF/context.xml
-sh apache-tomcat-9.0.104/bin/startup.sh
+yum install java-121-amazon-corretto -y
+wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.55/bin/apache-tomcat-10.1.55.tar.gz
+tar -zxvf apache-tomcat-10.1.55.tar.gz
+sed -i '55  a\<role rolename="manager-gui"/>' apache-tomcat-10.1.55/conf/tomcat-users.xml
+sed -i '56  a\<role rolename="manager-script"/>' apache-tomcat-10.1.55/conf/tomcat-users.xml
+sed -i '57  a\<user username="tomcat" password="admin@123" roles="manager-gui, manager-script"/>' apache-tomcat-10.1.55/conf/tomcat-users.xml
+sed -i '58  a\</tomcat-users>' apache-tomcat-10.1.55/conf/tomcat-users.xml
+sed -i '55d' apache-tomcat-10.1.55/conf/tomcat-users.xml
+sed -i '24d' apache-tomcat-10.1.55/webapps/manager/META-INF/context.xml
+sed -i '25d'  apache-tomcat-10.1.55/webapps/manager/META-INF/context.xml
+sh apache-tomcat-10.1.55/bin/startup.sh
